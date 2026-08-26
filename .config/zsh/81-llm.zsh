aa19b33648a33de4b79668b477a1192b8317f594
ajiankexx <ajiankex@163.com>
2026-08-26 16:31:29 +0800

    config: update some llm-related env vars

---
+++ .config/zsh/81-llm.zsh
@@ -0,0 +1,6 @@
+export HUOSHAN_AGENT_BASE_URL="https://ark.cn-beijing.volces.com/api/plan/v3"
+export HUOSHAN_CODING_BASE_URL="https://ark.cn-beijing.volces.com/api/coding/v3"
+export DEEPSEEK_BASE_URL="https://api.deepseek.com"
+export OPENAI_BASE_URL="https://api.openai.com/v1"
+
+source ~/.env.zsh
